8a759603a7471e9cbd0dd33dcb5b0d692b9515b2
Shirkanzilla <eichenrode@gmail.com>
2024-05-13 17:14:42 +0200

    eliminated valgrind errors

--- 2-wsort/wsort.c
+++ 2-wsort/wsort.c
@@ -4,13 +4,13 @@
 
 size_t using;
 
-char **readFile(FILE *fp){
-	char **arr=calloc(1,sizeof(char*));
+char **readFile(FILE *fp){//creates array of strings out of fp
+	char **arr=calloc(2,sizeof(char*));//create array of strings
 	size_t arrLength = 2;
-	using = 0;
+	using = 0;//current first free index of arr
 	char startOfLine = fgetc(fp);
 	while(startOfLine!=EOF){
-		if(using >= arrLength){
+		if(using >= arrLength){//double array size
 			arrLength *= 2;
 			arr = realloc(arr,arrLength*sizeof(char*));
 			if(arr == NULL) perror("realloc error");
@@ -20,38 +20,31 @@ char **readFile(FILE *fp){
 		if(s2 == NULL || s1 == NULL) perror("malloc error");
 		if(fgets(s2,100,fp) == NULL) perror("failed to read next line");
 		s1[0]=(char)startOfLine;
-		strcat(s1,s2);
+		strcat(s1,s2);//concat first char of line with the rest
 		free(s2);
-		arr[using]=calloc(101,sizeof(char*));
 		arr[using]=s1;
-		//free(s1);
 		using++;
 		startOfLine = fgetc(fp);
 	}
 	return arr;
 }
 
-int compare(const void *a, const void *b){
+int compare(const void *a, const void *b){//compare function for qsort
 	return strcmp(*(const char**)a, *(const char**)b);
 }
 
-char **sortArray(char **arr){
+char **sortArray(char **arr){//sorts array alphabetically
 	qsort(arr,using,sizeof(char*),compare);
 	return arr;
 }
 
-void delArray(char** arr){
+void delArray(char** arr){//frees the space reserved for the array
 	int i;
 	for(i=0;i<using;i++) free(arr[i]);
 	free(arr);
 }
 
 int main(int argc, char* argv[]){
-	//FILE *fp = fopen("wlist0","r");
-	//if(fp == NULL) {
-      	//	perror("Error in opening file");
-      	//	return(-1);
-   	//}
 	char **arr= sortArray(readFile(stdin));
 	int i;
 	for(i=0;i<using;i++){
